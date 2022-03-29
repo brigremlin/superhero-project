9f435e3c287568b5d556d9aafbad13af44f32a73
Brianna Grimsley <brigrimsley@gmail.com>
2022-03-29 11:10:56 -0500

    added login

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -5,7 +5,8 @@ class SuperheroesController < ApplicationController
     end
 
     def index
-        @superheroes = Superhero.all
+        @superheroes = Superhero.paginate(page: params[:page], per_page: 12)
+
     end
 
     def new 
