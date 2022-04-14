1892da86f262504389eb7064f85761d4c0e4ff7d
Brianna Grimsley <brigrimsley@gmail.com>
2022-04-14 17:40:05 -0500

    bg: working on fixing universe attribute

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -1,12 +1,38 @@
 class UniversesController < ApplicationController
-    
+    before_action :require_admin, except: [:index, :show]
+
+    def new
+        @universe = Universe.new
+    end
+
+    def create
+        @universe = Universe.new(universe_params)
+        if @universe.save 
+            flash[:notice] = "Universe was successfully created"
+            redirect_to @universe
+        else
+            render 'new'
+        end
+    end
+  
     def index
-        @universes = Universe.paginate(page: params[:page], per_page: 5)
+        @Universes = Universe.paginate(page: params[:page], per_page: 5)
     end
   
     def show
         @universe = Universe.find(params[:id])
     end
-    
 
-end
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+          flash[:alert] = "Only admins can perform that action"
+          redirect_to universes_path
+        end
+      end
+
+    private 
+
+    def universe_params
+        params.require(:universe).permit(:name)
+    end
+  end 
--- app/models/universe.rb
+++ app/models/universe.rb
@@ -1,3 +1,4 @@
 class Universe < ApplicationRecord
-    has_many :superheroes
+    has_many :superhero_universes
+    has_many :superheroes, through: :superhero_universes
 end
